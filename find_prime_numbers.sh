300ce1d3457b11edbb3e5910577d0f8477d8018a
priyapradeepkumar <118641694+priyapradeepkumar@users.noreply.github.com>
2022-11-20 16:50:22 -0500

    Update find_prime_numbers.sh

--- find_prime_numbers.sh
+++ find_prime_numbers.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+ 
 
+echo "Enter the number greater than 2 to find all prime numbers until the given number"
 
-echo "Hello Priya"
+read n
+
+for ((i=2; i<=$n; i++))
+do
+	isprime=1
+
+	for ((j=2; j<$i; j++))
+	do
+		if [ $(($i % $j)) == 0 ];then
+			isprime=0
+			break	
+		fi
+
+	done
+
+	if [ $isprime == 1 ];then
+		prime_num+=($i)
+	fi
+
+done
+
+if [ ${#prime_num[@]} -ne 0 ];then
+	echo "\n"
+	echo "There are ${#prime_num[@]} prime numbers until the number $n, those are : \n"
+	echo "${prime_num[@]}"
+fi
